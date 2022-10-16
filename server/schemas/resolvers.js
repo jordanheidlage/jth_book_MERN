7ebc7b8bfc871b6cae9a493f794239a1a58d57fb
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-10-16 14:43:33 -0400

    added me Query type to return User type

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,14 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+              return User.findOne({ _id: context.user._id }).populate('thoughts');
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          },
+    }
+}
