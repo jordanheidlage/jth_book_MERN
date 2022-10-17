58aff70171ea24017123ce680b34150521be0797
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-10-17 10:19:29 -0400

    added book type

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,4 +8,13 @@ const typeDefs = gql`
         bookCount: String
         savedBooks: [Book]!
     }
+
+    type Book {
+        bookId: ID
+        authors: [String]!
+        description: String
+        title: String
+        image: String
+        link: String
+    }
 `
