3f7f223e51c502248793a4dbb85898891c2281c1
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-10-17 10:26:24 -0400

    adduser and login mutations added

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,4 +26,9 @@ const typeDefs = gql`
     type Query {
         me: User
     }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+    }
 `
