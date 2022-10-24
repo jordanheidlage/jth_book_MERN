d19ef53d194cc3c4908343693dbb0aa7f2aed86d
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-10-24 12:47:14 -0400

    import apollo provider to make request work with apollo server

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
+import { useQuery } from '@apollo/client'
+import { useMutation } from '@apollo/client';
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
