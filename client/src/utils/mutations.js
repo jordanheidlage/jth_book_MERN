8044fa42e15dcbcd617de51b8fa55d113f11c7b0
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-10-24 12:41:26 -0400

    added save book and remove book mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,39 @@ export const ADD_USER = gql`
     }
   }
 `;
+export const SAVE_BOOK = gql`
+mutation saveBook($save: BookMutations) {
+  saveBook(save: $save) {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}
+`;
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID) {
+  removeBook(bookId: $bookId) {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}
+`;
